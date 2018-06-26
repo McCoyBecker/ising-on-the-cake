7652b46ff774367eec8ba69a1dba722b28599547
McCoy Becker <mccoybecker@gmail.com>
2018-06-25 21:59:55 -0500

    Updated analysis().py

--- analysis().py
+++ analysis().py
@@ -14,11 +14,11 @@ import SimulationClass as simulation
 import DataAnalyzerClass as data
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
-#-----------------
+#-------------------
 # Distance and plot
-#-----------------
+#-------------------
 
-df = pd.read_csv('/Users/mccoybecker/equilibrated30by30_60temp.csv')
+df = pd.read_csv('/Users/mccoybecker/equilibrated30by30.csv')
 df = df.sort_values(by=['Temp'])
 TempDensityList= []
 for m in range(3):
@@ -41,11 +41,15 @@ plt.show()
 
 BootstrapParameter = input("Bootstrap number: ")
 rootList = []
+tempList = []
+
+for i in df['Temp']:
+    tempList.append(i)
 
 for n in range(BootstrapParameter):
     SamplefromPoints = []
     
-    for T in set(mySimulation.TemperatureList):
+    for T in set(tempList):
         Points = [j[1] for j in TempDensityList if j[0] == T]
         SamplefromPoints.append((T, random.choice(Points)))
     
@@ -53,14 +57,13 @@ for n in range(BootstrapParameter):
     T,Points = zip(*SamplefromPoints)
     LOESSestimates = lowess(Points,T,return_sorted=True)
     temp,LOESS = zip(*LOESSestimates)
-    spl = InterpolatedUnivariateSpline(temp, LOESS,k=4)
-    rootList.append(spl.derivative().roots()[1])
-
-plt.scatter(*zip(*LOESSestimates))
-plt.title('LOESS smoothing')
-plt.xlabel('Temperature')
-plt.ylabel('Distance from Voronoi boundary')
-plt.show()
+    spl = InterpolatedUnivariateSpline(temp, LOESS, k=4)
+    plt.scatter(temp,LOESS)
+    plt.plot(temp, spl(temp), 'g', lw=3, alpha=0.7)
+    plt.title('LOESS smoothing with univariate spline fit k=4')
+    plt.xlabel('Temperature')
+    plt.ylabel('Distance from Voronoi boundary')
+    plt.show()
 
 print(str(np.mean(rootList)) + " +/- " + str(np.std(rootList)))
 
